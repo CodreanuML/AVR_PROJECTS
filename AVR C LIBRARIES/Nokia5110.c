6fdb03a3bff7225dc215f8161cc3b2668ac50650
CodreanuML <102348157+CodreanuML@users.noreply.github.com>
2023-06-29 10:13:20 +0300

    Update Nokia5110.c

--- AVR C LIBRARIES/Nokia5110.c
+++ AVR C LIBRARIES/Nokia5110.c
@@ -5,6 +5,38 @@
  *  Author: mcodrea2
  */ 
 
+#include <avr/io.h>
+#include <stdio.h>
+#define RST 9
+#define CE  10
+#define DC  11
+#define DIN  12
+#define CLK  13
+#define HIGH 1
+#define LOW 0
+#define OUT 1
+#define IN 0
+
+//LCD init Function
+
+void LCD_INIT(){
+		 PinMode(RST, OUT);
+		 PinMode(CE, OUT);
+		 PinMode(DC, OUT);
+		 PinMode(DIN, OUT);
+		 PinMode(CLK, OUT);
+		 DigitalWrite(RST, LOW);
+		 DigitalWrite(RST, HIGH);
+		 
+		 LcdWriteCmd(0x21);  // LCD extended commands
+		 LcdWriteCmd(0xB0);  // set LCD Vop (contrast)
+		 LcdWriteCmd(0x04);  // set temp coefficent
+		 LcdWriteCmd(0x14);  // LCD bias mode 1:40
+		 LcdWriteCmd(0x20);  // LCD basic commands
+		 LcdWriteCmd(0x0C);  // LCD basic commands
+	
+}
+
 //function used to shift out bits to display
 void shiftOut(uint8_t dataPin, uint8_t clockPin,  uint8_t val)
 {
