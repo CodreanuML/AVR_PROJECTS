20e87329a1d604916c1dcc0143622e4a52cedde1
CodreanuML <102348157+CodreanuML@users.noreply.github.com>
2023-06-29 11:24:52 +0300

    Update Nokia5110.c

--- AVR C LIBRARIES/Nokia5110.c
+++ AVR C LIBRARIES/Nokia5110.c
@@ -5,6 +5,35 @@
  *  Author: mcodrea2
  */ 
 
+
+// NOKIA 5110 Display , CHECK DATA SHEET FOR COMMAND LIST
+/*
+
+Display Technology	Dot Matrix LCD
+MCU Interface	SPI
+Screen Size	1.5 Inch Across
+Resolution	84×48 pixels  - 6 LINES , 84 COLUMNS
+Operating Voltage	2.7V – 3.3V
+Operating Current	50mA max
+Viewing Angle	180°  */
+
+
+
+//PINOUT  FROM LEFT TO RIGHT
+
+/*
+1 RST - pin is used to reset the display. It is an active low pin, which means that by pulling it low, the display can be reset. By connecting this pin to the Arduino’s reset, the screen will be automatically reset.
+2 CE - Chip Enable -pin is used to select one of several connected modules that share the same SPI bus. It’s also an active low pin.
+3 D/C -Data / Command - is used by the library to separate the commands (such as setting the cursor to a specific location, clearing the screen, etc.) from the data.
+4 DIN - serial data pin
+5 CLK - serial clock pin
+6 VCC -LCD VCC
+7 BL - Blacklight pin controls the backlight of the display. By connecting this pin to any PWM-capable Arduino pin or by using a potentiometer, the brightness can be controlled.
+8 GND - GroundPin
+*/
+
+
+
 #include <avr/io.h>
 #include <stdio.h>
 #define RST 9
